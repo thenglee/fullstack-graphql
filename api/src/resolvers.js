c79aae2a8a4e9558ee8c074842eada3759905499
Tan Theng Lee <thengleetan@gmail.com>
2020-09-06 21:56:45 +0800

    Create input type for pets and pet queries

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,20 +5,16 @@
 
 module.exports = {
   Query: {
-    pets(_, __, { models }) {
-      //return models.Pet.findMany({})
-      return [
-        { id: 1, name: 'moose' },
-        { id: 2, name: 'garfield' }
-      ]
+    pets(_, { input }, { models }) {
+      return models.Pet.findMany(input)
+    },
+    pet(_, { id }, { models }) {
+      console.log('id', id)
+      return models.Pet.findOne({ id: id })
     }
   },
   //Mutation: {},
   Pet: {
-    id(pet) {
-      console.log(pet)
-      return 3
-    }
     //img(pet) {
     //  return pet.type === 'DOG'
     //    ? 'https://placedog.net/300/300'
