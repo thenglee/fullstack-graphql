0005d6a17a796a1dc85b90425a65ec6694dfde01
Tan Theng Lee <thengleetan@gmail.com>
2020-09-06 13:05:27 +0800

    Fill up Pet schema and query resolver

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,25 @@
 
 module.exports = {
   Query: {
-    
-  },
-  Mutation: {
-    
-  },
-  Pet: {
-    img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
+    pets(_, __, { models }) {
+      //return models.Pet.findMany({})
+      return [
+        { id: 1, name: 'moose' },
+        { id: 2, name: 'garfield' }
+      ]
     }
   },
-  User: {
-    
-  }
+  //Mutation: {},
+  Pet: {
+    id(pet) {
+      console.log(pet)
+      return 3
+    }
+    //img(pet) {
+    //  return pet.type === 'DOG'
+    //    ? 'https://placedog.net/300/300'
+    //    : 'http://placekitten.com/300/300'
+    //}
+  },
+  User: {}
 }
