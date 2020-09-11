e02bc51c5905b6c6a42c07c970ecdc4109ac753f
Tan Theng Lee <thengleetan@gmail.com>
2020-09-11 22:37:21 +0800

    Add optimisticResponse to addPet mutation

--- client/src/client.js
+++ client/src/client.js
@@ -1,13 +1,27 @@
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloLink } from 'apollo-link'
 import { HttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
 import gql from 'graphql-tag'
 
 /**
  * Create a new apollo client and export as default
  */
 
-const link = new HttpLink({ uri: 'http://localhost:4000/' })
+const delay = setContext(
+  request =>
+    new Promise((success, fail) => {
+      setTimeout(() => {
+        success()
+      }, 3800)
+    })
+)
+
+const http = new HttpLink({ uri: 'http://localhost:4000/' })
+
+const link = ApolloLink.from([delay, http])
+
 const cache = new InMemoryCache()
 
 const client = new ApolloClient({ link, cache })
